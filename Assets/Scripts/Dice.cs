4e6ef25f12e85c095e6bc8d3e2745bf4e7e437f4
alucia <andrew99aj9@gmail.com>
2022-07-16 17:26:18 -0400

    Prefabs for Dice and Player

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using UnityEngine;
 
@@ -62,3 +63,64 @@ public class Dice : MonoBehaviour {
         Debug.Log(rollable);
     }
 }
+=======
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+    // Array of dice sides sprites to load from Resources folder
+    public Sprite[] diceSides;
+
+    // Reference to sprite renderer to change sprites
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	private void Start () {
+
+        // Assign Renderer component
+        rend = GetComponent<SpriteRenderer>();
+
+        // Load dice sides sprites to array from DiceSides subfolder of Resources folder
+        //diceSides = Resources.LoadAll<Sprite>("/Images/Dice/");
+	}
+	
+    // If you left click over the dice then RollTheDice coroutine is started
+    private void OnMouseDown()
+    {
+        StartCoroutine("RollTheDice");
+    }
+
+    // Coroutine that rolls the dice
+    private IEnumerator RollTheDice()
+    {
+        // Variable to contain random dice side number.
+        // It needs to be assigned. Let it be 0 initially
+        int randomDiceSide = 0;
+
+        // Final side or value that dice reads in the end of coroutine
+        int finalSide = 0;
+
+        // Loop to switch dice sides ramdomly
+        // before final side appears. 20 itterations here.
+        for (int i = 0; i <= 20; i++)
+        {
+            // Pick up random value from 0 to 5 (All inclusive)
+            randomDiceSide = Random.Range(0, diceSides.Length);
+
+            // Set sprite to upper face of dice from array according to random value
+            rend.sprite = diceSides[randomDiceSide];
+
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        // Assigning final side so you can use this value later in your game
+        // for player movement for example
+        finalSide = randomDiceSide + 1;
+
+        // Show final dice value in Console
+        Debug.Log(finalSide);
+    }
+}
+>>>>>>> bb92d3a3e7a724348c9f608ad447f15d587fd1ec
